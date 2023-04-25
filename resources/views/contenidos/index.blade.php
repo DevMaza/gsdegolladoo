044db294d0b3c140766d4e0f23efb9202574c97a
DevMaza <l17271332@tuxtla.tecnm.mx>
2023-04-24 23:46:40 -0600

    modifique index contenido

--- resources/views/contenidos/index.blade.php
+++ resources/views/contenidos/index.blade.php
@@ -29,32 +29,37 @@
                               <tbody>
                             @foreach ($contenidos as $contenido)
                             <tr>
-                                
-                                <td style="display: none;">{{ $contenido->id }}</td>                                
-                                <td>{{ $contenido->titulo }}</td>
-                                <td>{{ $contenido->contenido }}</td>
-                                
-                                <td>
-                                    <form action="{{ route('contenidos.destroy',$contenido->id) }}" method="POST">                                        
-                                        @can('editar-contenido')
-                                        <a class="btn btn-info" href="{{ route('contenidos.edit',$contenido->id) }}">Editar</a>
-                                        @endcan
-
-                                                        @can('ver-contenido')
-                                                        <a class="btn btn-success" href="{{ route('contenidos.show',$contenido->id) }}">ver</a>
+                                @foreach ($users as $user)
+                                    @if($user->name == \Illuminate\Support\Facades\Auth::user()->name)
+                                            @if($contenido->grupo_id  == $user->grupo_id)                                
+                                                <td style="display: none;">{{ $contenido->id }}</td>                                
+                                                <td>{{ $contenido->titulo }}</td>
+                                                <td>{{ $contenido->contenido }}</td>
+                                                
+                                                <td>
+                                                    <form action="{{ route('contenidos.destroy',$contenido->id) }}" method="POST">                                        
+                                                        @can('editar-contenido')
+                                                        <a class="btn btn-info" href="{{ route('contenidos.edit',$contenido->id) }}">Editar</a>
                                                         @endcan
 
+                                                                        @can('ver-contenido')
+                                                                        <a class="btn btn-success" href="{{ route('contenidos.show',$contenido->id) }}">ver</a>
+                                                                        @endcan
 
-                                        @csrf
-                                        @method('DELETE')
-                                        @can('borrar-contenido')
-                                        <button type="submit" class="btn btn-danger">Borrar</button>
-                                        @endcan
-                                    </form>
-                                </td>
-                                @role('administrador|Docente')
-                                <td><img height="100px"  src="{{asset('storage'.'/'.$contenido->imagen)}}" alt="{{$contenido->title}}"class="img-fluid" width="100px"></td>	
-                                @endrole
+
+                                                        @csrf
+                                                        @method('DELETE')
+                                                        @can('borrar-contenido')
+                                                        <button type="submit" class="btn btn-danger">Borrar</button>
+                                                        @endcan
+                                                    </form>
+                                                </td>
+                                                @role('administrador|Docente')
+                                                <td><img height="100px"  src="{{asset('storage'.'/'.$contenido->imagen)}}" alt="{{$contenido->title}}"class="img-fluid" width="100px"></td>	
+                                                @endrole
+                                        @endif
+                                    @endif
+                             @endforeach
                             </tr>
                             @endforeach
                             </tbody>
