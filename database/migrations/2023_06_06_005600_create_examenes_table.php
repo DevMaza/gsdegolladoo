21acde3fb601fac702b5f584ab20eb7f9175488a
DevMaza <l17271332@tuxtla.tecnm.mx>
2023-06-07 10:32:32 -0600

    add migracion exa y pre

---
+++ database/migrations/2023_06_06_005600_create_examenes_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('examenes', function (Blueprint $table) {
+            $table->id();
+            $table->string('titulo');
+            $table->text('descripcion');
+            $table->unsignedBigInteger('grupo_id');
+            $table->foreign('grupo_id')->references('id')->on('grupos')->onDelete('cascade');
+            $table->unsignedBigInteger('materia_id');
+            $table->foreign('materia_id')->references('id')->on('materias')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('examenes');
+    }
+};
---
+++ database/migrations/2023_06_06_005624_create_preguntas_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('preguntas', function (Blueprint $table) {
+            $table->id();
+            $table->string('pregunta');
+            $table->string('incisoa');
+            $table->string('incisob');
+            $table->string('incisoc');
+            $table->string('respuesta');
+            $table->unsignedBigInteger('examene_id');
+            $table->foreign('examene_id')->references('id')->on('examenes')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('preguntas');
+    }
+};
